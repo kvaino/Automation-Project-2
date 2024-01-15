7f22b9715ecdaf2130df4a10d2831668a5deaa1a
kvaino <kristel.vainomets@gmail.com>
2024-01-15 12:29:31 +0200

    Sprint2 Sample solution files

---
+++ cypress/constants.js
@@ -0,0 +1,4 @@
+export const issueDescription = 'User can add and modify estimated time of issue';
+export const issueTitle = 'Time Estimation/Logging Test';
+export const issueCreatedConfirmation = 'Issue has been successfully created.';
+
---
+++ cypress/pages/selectors.js
@@ -0,0 +1,5 @@
+export const backLogList = '[data-testid="board-list:backlog"]';
+export const inputFieldTime = 'input[placeholder="Number"]';
+export const timeTrackingModal = '[data-testid="modal:tracking"]';
+export const timeTrackingButton = '[data-testid="icon:stopwatch"]';
+
